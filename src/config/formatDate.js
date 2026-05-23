a1e06d9b062878d89cf21efacab071c7078918be
Ayesha Ansari <ayeshansari124@gmail.com>
2026-05-23 15:02:09 +0530

    fix: time formatting

--- src/config/formatDate.js
+++ src/config/formatDate.js
@@ -1,7 +1,13 @@
 const dayjs = require("dayjs");
 
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 const formatDate = (date) => {
-  return dayjs(date).format("DD MMM YY • hh:mm A");
+  return dayjs(date).tz("Asia/Kolkata").format("DD MMM YY • hh:mm A");
 };
 
 module.exports = formatDate;
